73677d6df67eb5d3891e75b1d3646bd8eec38cf5
Tak <tafutada@gmail.com>
2021-02-11 11:50:47 +0900

    save

--- README.md
+++ README.md
@@ -2,9 +2,17 @@
 
 Get myself familier with C#, which I feel it's a bit similar to Kotlin.
 
-## Functionalities to exam
+## C# Functionalities to exam
 
 Regex, Concurrency, Parallel, inheritance
+
+### async/await
+
+Cooking breakfast is a good example of asynchronous work that isn't parallel. One person (or thread) can handle all these tasks.
+
+### Parallel.ForEach
+
+For a parallel algorithm, you'd need multiple cooks (or threads). One would make the eggs, one the bacon, and so on. Each one would be focused on just that one task. Each cook (or thread) would be blocked synchronously waiting for bacon to be ready to flip, or the toast to pop.
 
 ## async/wait vs Parallel
 
--- Regex1.cs
+++ Regex1.cs
@@ -13,40 +13,56 @@ public class Test
     public static void Main()
     {
         var stories = new List<string>(){
-            "2021年2月9日 イーロンマスクがビットコインを買った",
+            "2021年2月9日 イーロンマスクがリップルを、1,234,567円買った",
             "GameStopがドージコインを売った 2月10日(水)",
-            "2021/02/01 ロビンフッドがドージコインを売った",
-            "令和3年 税制改革",
-            "日経平均 3万円 230円高",
-            "ドル円",
-            "アメリカ　バイデン　金融緩和 ",
-            "アメリカ　国債 ",
+            "2021/02/01 ロビンフッドがドージコインを¥1.23売った",
+            "トヨタ自動車、3Q累計税引前1,869,989百万。",
+            "10日の東京株式市場で日経平均株価は4日続伸し、前日比57円00銭（0.19%）高の2万9562円93銭で終えた",
+            "14時時点は1ﾄﾞﾙ=104円56銭近辺と、前日17時時点と比べ21銭の円高・ドル安だった",
+            "２０２１年度一般会計税収は、２０年度当初予算比９．５％減の５７兆４４８０億円となる見込みだ",
         };
-
-        var st = new SystemTrader();
 
         var tm = new Stopwatch();
         tm.Start();
+        var st = new SystemTrader();
+        var tasks = new List<Task>();
 
-        foreach (string story in stories)
+        foreach (var v in stories)
         {
-            st.ParseTweet(story);
+            var story = v;
+
+            tasks.Add(Task.Run(() =>
+            {
+                st.ParseTweet(story); // spawn a heavy job 
+            })); // like go func but thread
         }
-        tm.Stop(); Console.WriteLine("Sequential: {0}", tm.Elapsed);
 
-        // Parallel
-        tm.Reset(); tm.Start();
+        Console.WriteLine("a: {0}", tm.Elapsed);
 
-        var po = new ParallelOptions() { MaxDegreeOfParallelism = Environment.ProcessorCount };
-        Parallel.ForEach(stories, po, story =>
+        Task t = Task.WhenAll(tasks);
+
+        Console.WriteLine("b: {0}", tm.Elapsed);
+
+        try
         {
-            st.ParseTweet(story);
-        });
-        tm.Stop(); Console.WriteLine("Parallel: {0}", tm.Elapsed);
+            t.Wait(); // wait all
+        }
+        catch { }
+
+        Console.WriteLine("c: {0}", tm.Elapsed);
+
+
+        // // Parallel
+        // tm.Reset(); tm.Start();
+
+        // var po = new ParallelOptions() { MaxDegreeOfParallelism = Environment.ProcessorCount };
+        // Parallel.ForEach(stories, po, story =>
+        // {
+        //     st.ParseTweet(story);
+        // });
+        // tm.Stop(); Console.WriteLine("Parallel: {0}", tm.Elapsed);
 
     }
-
-
     // study how to use Regex to parse date, digit and currency
     private class SystemTrader
     {
