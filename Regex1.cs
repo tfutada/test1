ea893d4ed68625d455688709e53611773a3b7187
Tak <tafutada@gmail.com>
2021-02-11 12:26:19 +0900

    save

--- Regex1.cs
+++ Regex1.cs
@@ -13,10 +13,10 @@ public class Test
     public static void Main()
     {
         var stories = new List<string>(){
-            "2021年2月9日 イーロンマスクがリップルを、1,234,567円買った",
+            "2021年2月9日 イーロンマスクがリップルを、1,２３４,567円買った",
             "GameStopがドージコインを売った 2月10日(水)",
             "2021/02/01 ロビンフッドがドージコインを¥1.23売った",
-            "トヨタ自動車、3Q累計税引前1,869,989百万。",
+            "トヨタ自動車、3Q累計税引前1,869,989百万円。",
             "10日の東京株式市場で日経平均株価は4日続伸し、前日比57円00銭（0.19%）高の2万9562円93銭で終えた",
             "14時時点は1ﾄﾞﾙ=104円56銭近辺と、前日17時時点と比べ21銭の円高・ドル安だった",
             "２０２１年度一般会計税収は、２０年度当初予算比９．５％減の５７兆４４８０億円となる見込みだ",
@@ -27,13 +27,14 @@ public class Test
         var st = new SystemTrader();
         var tasks = new List<Task>();
 
+        // parse each story in parallel
         foreach (var v in stories)
         {
             var story = v;
 
             tasks.Add(Task.Run(() =>
             {
-                st.ParseTweet(story); // spawn a heavy job 
+                st.ParseTweet(story); // spawn a heavy job
             })); // like go func but thread
         }
 
@@ -66,13 +67,14 @@ public class Test
     // study how to use Regex to parse date, digit and currency
     private class SystemTrader
     {
-        private static Regex re = new Regex(@"^イーロンマスクが(.+)を(.+)った$", RegexOptions.Compiled);
+        private static Regex currency1 = new Regex(@"[\d,兆億百万]+円\d*銭?", RegexOptions.Compiled);
+        private static Regex currency2 = new Regex(@"¥[\d,\.兆億百万]+", RegexOptions.Compiled);
         public void ParseTweet(String tweet)
         {
-            Match mt = re.Match(tweet);
+            Match mt = currency2.Match(tweet);
             if (mt.Success)
             {
-                Console.WriteLine("{0}:{1}", mt.Groups[1].Value, mt.Groups[2].Value);
+                Console.WriteLine("Currency: {0}", mt.Groups[0].Value);
             }
         }
     }
