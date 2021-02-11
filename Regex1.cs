7721c52be914e6c65bb1bb20b12df995bbd4f850
Tak <tafutada@gmail.com>
2021-02-11 16:33:58 +0900

    save

--- Regex1.cs
+++ Regex1.cs
@@ -10,11 +10,15 @@ using System.Threading.Tasks;
 
 public class Test
 {
+    public interface IParse
+    {
+        public void ParseTweet(string tweet);
+    }
     public static void Main()
     {
         var stories = new List<string>(){
             "2021年2月9日 イーロンマスクがリップルを、1,２３４,567円買った",
-            "GameStopがドージコインを売った 2月10日(水)",
+            "abc令和2年3月8日GameStopがドージコインを売った",
             "2021/02/01 ロビンフッドがドージコインを¥1.23売った",
             "トヨタ自動車、3Q累計税引前1,869,989百万円。",
             "10日の東京株式市場で日経平均株価は4日続伸し、前日比57円00銭（0.19%）高の2万9562円93銭で終えた",
@@ -24,7 +28,7 @@ public class Test
 
         var tm = new Stopwatch();
         tm.Start();
-        var st = new SystemTrader();
+        IParse st = new SystemTrader();
         var tasks = new List<Task>();
 
         // parse each story in parallel
@@ -38,11 +42,11 @@ public class Test
             })); // like go func but thread
         }
 
-        Console.WriteLine("a: {0}", tm.Elapsed);
+        // Console.WriteLine("a: {0}", tm.Elapsed);
 
         Task t = Task.WhenAll(tasks);
 
-        Console.WriteLine("b: {0}", tm.Elapsed);
+        // Console.WriteLine("b: {0}", tm.Elapsed);
 
         try
         {
@@ -50,10 +54,10 @@ public class Test
         }
         catch { }
 
-        Console.WriteLine("c: {0}", tm.Elapsed);
+        // Console.WriteLine("c: {0}", tm.Elapsed);
     }
     // study how to use Regex to parse date, digit and currency
-    private class SystemTrader
+    private class SystemTrader : IParse
     {
         private List<Regex> regexes;
         private static ParallelOptions po = new ParallelOptions() { MaxDegreeOfParallelism = Environment.ProcessorCount };
@@ -62,18 +66,19 @@ public class Test
         {
             regexes = new List<Regex>(){
                 new Regex(@"[\d,兆億百万]+円\d*銭?", RegexOptions.Compiled),
-                new Regex(@"¥[\d,\.兆億百万]+", RegexOptions.Compiled)
+                new Regex(@"¥[\d,\.兆億百万]+", RegexOptions.Compiled),
+                new Regex(@"(昭和|平成|令和)\d*年?\d*月?\d*日?", RegexOptions.Compiled),
+                // new Regex(@"(\d+年\d+月\d+日|\d+年\d+月|\d+年)", RegexOptions.Compiled),
             };
         }
         public void ParseTweet(String tweet)
         {
-
             Parallel.ForEach(regexes, po, re =>
             {
-                Match mt = re.Match(tweet);
-                if (mt.Success)
+                MatchCollection matches = re.Matches(tweet);
+                foreach (Match m in matches)
                 {
-                    Console.WriteLine("Currency: {0}", mt.Groups[0].Value);
+                    Console.WriteLine("matched: {0}", m.Value);
                 }
             });
         }
